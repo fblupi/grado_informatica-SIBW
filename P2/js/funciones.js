a8c4a79655e4654b0bd0e982cad117286d703be0
MiKe <coquert@gmail.com>
2015-04-06 19:31:48 +0200

    Nuevo slider con transición de opacidad.

--- P2/js/funciones.js
+++ P2/js/funciones.js
@@ -15,6 +15,21 @@ function rotarSlider(id, contenedor, numElementos, width, velocidad) {
     var timer = setInterval(rotacion, velocidad);
 }
 
+function slide(clase,id,tiempoTrans){
+    //En el css la clase 'clase' debe aparecer como display none. El id 'id' es el que marca qué imagen del slider es la que se muestra que puede tener cualquier estilo. En el html, la primera imagen debe ser la que tenga el id que indica que está activo.
+    
+    var imagenes = document.getElementsByClassName(clase);
+    var cont=0;
+    var tama=imagenes.length;    
+    
+    setInterval(function(){        
+        imagenes[cont].removeAttribute('id');
+        cont=(cont+1)%tama;
+        imagenes[cont].setAttribute('id',id);
+        
+    },tiempoTrans);
+}
+
 function validar(nombreFormulario) {
     var formulario = document.forms[nombreFormulario],
         validado = true,
