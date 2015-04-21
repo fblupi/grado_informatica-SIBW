5504fc28cff90e82a5c05ee6f4f72daeaa431919
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2015-04-21 16:00:44 +0200

    primeras ponencias

---
+++ P3/contenido/ponencias/informatica-grafica/visualizacion-y-realismo.php
@@ -0,0 +1,8 @@
+<div id="content">
+    <section>
+        <article> 
+            <h1>Visualización y Realismo, por C. Ureña</h1>
+            <p>El concepto de realismo aplicado a la generación de imágenes de síntesis es uno de los elementos de la informática gráfica cuya evolución ha sido más visible. Desde la eliminación de las partes ocultas de los 70 hasta las estrategias basadas en la verdadera simulación del comportamiento físico del transporte de la luz; hemos transitado por los llamados modelos empíricos, los transicionales que incorporaban los fenómenos ópticos, los analíticos basados en el concepto de equilibrio energético. Como en otros ámbitos de la investigación, probablemente en todos los que lo son de verdad, los avances en la generación de imágenes de síntesis más y más realistas ha llevado a resultados que han inspirado nuevos temas de investigación y han abierto nuevos campos de aplicación de dichos resultados o de las técnicas concebidas para alcanzarlos</p>
+        </article>
+    </section>
+</div> <!-- end content -->
---
+++ P3/contenido/ponencias/ingenieria-del-software/ifml.php
@@ -0,0 +1,9 @@
+<div id="content">
+    <section>
+        <article> 
+            <h1>IFML, por M. Cabrera</h1>
+            <p>IFML (Interaction Flow Modeling Language) es un lenguaje de modelado estándar en el campo de la ingeniería de software. IFML incluye un conjunto de notaciones gráficas para crear modelos visuales de las interacciones del usuario y el comportamiento de aplicaciones para el usuario en los sistemas de software.
+IFML fue desarrollado en 2012 y 2013 bajo la dirección de WebRatio y fue inspirado por la notación WebML, así como por algunas otras experiencias en el campo de la modelización Web.</p>
+        </article>
+    </section>
+</div> <!-- end content -->
