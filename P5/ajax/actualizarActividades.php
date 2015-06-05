30bc39d19c02d1fd8a94f10364d205245d4b6b57
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2015-06-05 17:22:24 +0200

    Cuotas-actividades por defecto #37

--- P5/ajax/actualizarActividades.php
+++ P5/ajax/actualizarActividades.php
@@ -6,26 +6,30 @@
     $seleccion="SELECT ID_act,Titulo,Minifoto_src,Foto_title, Foto_alt FROM actividades";
     $resultado=mysql_query ($seleccion, $conexion);
     while($fila=mysql_fetch_array($resultado)) {
-        echo '<li class="actividades-form">';
-        echo '<label for="'.$fila["ID_act"].'">'.$fila["Titulo"].'</label>';
-        echo '<input type="checkbox" id="'.$fila["ID_act"].'" name="actividad[]" value="'.$fila["ID_act"].'" onchange="actualizarFoto(\''.$fila["ID_act"].'\')"/>';
-        echo '<div id="divpic-'.$fila["ID_act"].'"></div>';
-        echo '</li>';
-    }    
-    
-    $seleccion="SELECT ID_act FROM cuota_actividad WHERE ID_cuota='$cuota'"; // Selecciona las actividades que se van a marcar
-    $resultado=mysql_query ($seleccion, $conexion);
-    if(mysql_num_rows($resultado)>0){
-        while($fila=mysql_fetch_array($resultado)) {
-            $actividad=$fila["ID_act"];
-            //echo '<p>Meto '.$actividad.'</p>';
-            echo '<script type="text/javascript">';
-            echo '  alert("Lanzo script");';
-            echo '  marcar(\''.$actividad.'\');';
-            echo '  actualizarFoto(\''.$actividad.'\');';
-            echo '</script>';
+        $encontrado=false;
+        $seleccionDefault="SELECT ID_act FROM cuota_actividad WHERE ID_cuota='$cuota'"; // Selecciona las actividades que se van a marcar
+        $resultadoDefault=mysql_query ($seleccionDefault, $conexion);
+        if(mysql_num_rows($resultadoDefault)>0){
+            while($filaDefault=mysql_fetch_array($resultadoDefault) and !$encontrado) {
+                if($filaDefault["ID_act"]==$fila["ID_act"]) {
+                    $encontrado=true;
+                }
+            }
         }
-    }
-    
+        if($encontrado) {
+            echo '<li class="actividades-form">';
+            echo '<label for="'.$fila["ID_act"].'">'.$fila["Titulo"].'</label>';
+            echo '<input type="checkbox" id="'.$fila["ID_act"].'" name="actividad[]" value="'.$fila["ID_act"].'" disabled="true" checked="true" />';
+            echo '<div id="divpic-'.$fila["ID_act"].'"><img class="pic-actividad" id="pic-'.$fila["ID_act"].'" alt="'.$fila["Foto_alt"].'" title='.$fila["Foto_title"].'" src="'.$fila["Minifoto_src"].'"/></div>';
+            echo '</li>';
+        } else {
+            echo '<li class="actividades-form">';
+            echo '<label for="'.$fila["ID_act"].'">'.$fila["Titulo"].'</label>';
+            echo '<input type="checkbox" id="'.$fila["ID_act"].'" name="actividad[]" value="'.$fila["ID_act"].'" onchange="actualizarFoto(\''.$fila["ID_act"].'\')"/>';
+            echo '<div id="divpic-'.$fila["ID_act"].'"></div>';
+            echo '</li>';
+        }
+    }  
+
     mysql_close();
 ?>
